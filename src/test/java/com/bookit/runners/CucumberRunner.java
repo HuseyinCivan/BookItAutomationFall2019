afa52137ef41fb24dfe45e62dc2247c71a274009
Huseyin Civan <huseyincivan@gmail.com>
2020-06-07 14:03:37 -0400

    updated

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -1,24 +1,20 @@
 package com.bookit.runners;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "com\\bookit\\step_definitions",
-        features = "src\\test\\resources\\features",
+        glue = "com/bookit/step_definitions",
+        features = "src/test/resources/features",
         plugin = {
                 "json:target/cucumber.json"
+
         },
-        tags = "@ui",
+//        tags = "@ui",
         dryRun = false
-
-
-
 
 )
 public class CucumberRunner {
-
 }
